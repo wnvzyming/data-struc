68d7847a2ab0ebfce847d452fc5570fb64f8b399
wnvzyming <wnvzyming@sina.com>
2021-12-04 11:01:55 +0800

    balance

---
+++ leecode/example/main/hash.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"data-struc/leecode/example/balance"
+	"fmt"
+	"hash/crc32"
+	"math/rand"
+)
+
+type HashBalance struct {
+	Name string
+	Age  int
+}
+
+func init() {
+	balance.RegisterBalancer("hash", &HashBalance{})
+}
+func (p *HashBalance) DoBalance(insts []*balance.Instance, key ...string) (inst *balance.Instance, err error) {
+	var defKey string = fmt.Sprintf("%d", rand.Int())
+	if len(key) > 0 {
+		defKey = key[0]
+	}
+	lens := len(insts)
+	if lens == 0 {
+		err = fmt.Errorf("No backend instance")
+		return
+	}
+	crcTable := crc32.MakeTable(crc32.IEEE)
+	hashVal := crc32.Checksum([]byte(defKey), crcTable)
+	index := int(hashVal) % lens
+	inst = insts[index]
+	return
+}
